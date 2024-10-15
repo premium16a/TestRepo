00cee033028d086027266039c270fe0b48bf6ea7
premium16a <premium16a@gmail.com>
2024-10-15 11:49:04 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is a test project to share on Github using Git!");
+
+        System.out.println("This is a change from Github repository!"); 
     }
 }
